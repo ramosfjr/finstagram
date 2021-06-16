f3253eee7f2a6f9bcd73f1d000eff9caf3e312c8
ramosfjr <ramosfjr.3@gmail.com>
2021-06-16 01:01:51 +0000

    <W4D2>

--- app/actions.rb
+++ app/actions.rb
@@ -9,5 +9,16 @@ get '/signup' do
 end
 
 post '/signup' do
-  params.to_s
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
 end
